b7ff56367ba13ef08796df641ad9c8dfd1e8e117
김현희 <tta_bi@naver.com>
2021-12-23 09:46:26 +0900

    필요 변수 우선 세팅

--- 01_repeat_test_hard_06.py
+++ 01_repeat_test_hard_06.py
@@ -2,7 +2,14 @@
 # ex. 두 개의 정수 입력
 # ex. 최소 공배수를 저장해 둘 변수
 
+num1 = int(input('첫 번째 숫자 입력 : '))
+num2 = int(input('두 번째 숫자 입력 : '))
 
+# 최소 공배수(least common multiple)
+lcm = 0
+
+# 최대 공약수(greatest common factor)
+gcf = 0
 
 # 결과 변수에 올바른 답을 저장하기 위한 로직
 # ex. for / while 등 반복문, if / elif / else 조건문 사용
